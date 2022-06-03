08bd4fcae19ac66090214003778d9c6b5f22c844
joaoguilherme1 <joaoguilhermesouza1@gmail.com>
2022-06-03 11:27:22 -0300

    Adding components

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIelement/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavegation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
+    <React.Fragment>
+    { drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+    <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+        </nav>
+    </SideDrawer>
+
+
     <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
             <span />
             <span />
             <span />
@@ -16,9 +38,10 @@ const MainNavegation = props => {
             <Link to ="/">Your places</Link>
         </h1>
         <nav>
-            ...
+            <NavLinks />
         </nav>
     </MainHeader>
+    </React.Fragment>
     );
 };
 
